91c0c7087c2d495f83180434c2e4f8b2ec33de6d
skruegel2 <96012395+skruegel2@users.noreply.github.com>
2023-03-08 08:08:16 -0600

    LMS Color matching functions

--- Colorimetry/Section2.py
+++ Colorimetry/Section2.py
@@ -14,6 +14,7 @@ def create_wavelength():
     return wavelength
 
 def plot_wavelength(data, wavelength):
+    plt.clf
     plt.title("XYZ Color Matching Functions")
     plt.xlabel("Wavelength [nm]")
     plt.ylabel("Value")
@@ -23,12 +24,36 @@ def plot_wavelength(data, wavelength):
     plt.legend(['x', 'y', 'z'])
     plt.show()
 
+def plot_lms(lms, wavelength):
+    plt.clf
+    plt.title("LMS Color Matching Functions")
+    plt.xlabel("Wavelength [nm]")
+    plt.ylabel("Value")
+    plt.plot(wavelength[0,:],lms[0,:])
+    plt.plot(wavelength[0,:],lms[1,:])
+    plt.plot(wavelength[0,:],lms[2,:])
+    plt.legend(['l', 'm', 's'])
+    plt.show()
+
 # Load data.npy
 data = np.load('.\CIE_data\data.npy', allow_pickle=True)[()]
 # List keys of dataset
 data.keys()
 wavelength = create_wavelength()
-plot_wavelength(data, wavelength)
+#plot_wavelength(data, wavelength)
+
+A_inv = np.array([[0.243,0.856,-0.044],
+                 [-0.3910,1.1650,0.0870],
+                 [0.01,-0.008,0.5630]])
+
+cie_1931 = np.zeros((3,31))
+
+cie_1931[0,:] = data['x'][0,:]
+cie_1931[1,:] = data['y'][0,:]
+cie_1931[2,:] = data['z'][0,:]
+
+lms = np.matmul(A_inv,cie_1931)
+plot_lms(lms, wavelength)
 
 #plt.plot(X[0,:], X[1,:],'.')
 #plt.axis('equal')
