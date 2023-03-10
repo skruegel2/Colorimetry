bca2713c2ca75c9ca527485de3be1d442af5e77c
skruegel2 <96012395+skruegel2@users.noreply.github.com>
2023-03-09 23:23:42 -0600

    Gamma correct

--- Colorimetry/Section5.py
+++ Colorimetry/Section5.py
@@ -21,6 +21,20 @@ def remove_negative(RGB):
                     RGB[col_idx, row_idx, 2] = 1
     return RGB
 
+def gamma_correct(RGB, gamma):
+    for row_idx in range(RGB.shape[0]):
+        for col_idx in range(RGB.shape[1]):
+            for color_idx in range(3):
+                RGB[row_idx,col_idx,color_idx] = pow(RGB[row_idx,col_idx,color_idx],1/gamma)
+    return RGB
+
+def scale_image(RGB):
+    RGB_int = np.zeros(RGB.shape,np.uint8)
+    for row_idx in range(RGB.shape[0]):
+        for col_idx in range(RGB.shape[1]):
+            for color_idx in range(3):
+                RGB_int[row_idx,col_idx,color_idx] = np.uint8(255*RGB[row_idx,col_idx,color_idx])
+
 x_dim = np.arange(0, 1, 0.005)
 y_dim = np.arange(0, 1, 0.005)
 
@@ -33,6 +47,7 @@ M = [[0.640, 0.300, 0.150],
 RGB = np.zeros((200, 200, 3))
 RGB = compute_RGB(RGB, x, y, z, M)
 RGB = remove_negative(RGB)
-plt.imshow(RGB)
+RGB = gamma_correct(RGB,2.2)
+plt.imshow(RGB,extent=[0,1,0,1])
 plt.show()
 temp = 5
